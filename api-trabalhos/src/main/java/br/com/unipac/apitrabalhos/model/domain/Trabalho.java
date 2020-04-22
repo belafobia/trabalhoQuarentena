2da2a08226ffdaa93e39653d5f8b70a784531d5f
Isabela Diniz <isabela-cefetmg@hotmail.com>
2020-04-22 19:29:33 -0300

    Projeto

---
+++ api-trabalhos/src/main/java/br/com/unipac/apitrabalhos/model/domain/Trabalho.java
@@ -0,0 +1,33 @@
+package br.com.unipac.apitrabalhos.model.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+// api trabalho by Isabela Diniz
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_trabalho")
+public class Trabalho {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_professor")
+    private Professor professor;
+
+    @Column(name = "titulo_do_trabalho")
+    private String tituloDoTrabalho;
+
+    private Date data;
+
+}
